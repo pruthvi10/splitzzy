55ccf6e1669ac76aa1db76a59e5989a94dfb237f
pruthvi10 <pruthvimohanlal10@gmail.com>
2020-10-07 11:05:55 +0100

    line changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,9 @@ function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
+      <br />
       <h5>Second commit</h5>
+      <br />
       <p>slowly learning how git resp works :)</p>
     </div>
   );
